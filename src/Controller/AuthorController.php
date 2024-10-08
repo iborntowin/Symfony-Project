aabde378e818a0ad6b32d43a6f24aa378a3d9e27
iborntowin <nassim28maaoui@gmail.com>
2024-10-08 19:14:26 +0100

    Added data base

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,50 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuthorController extends AbstractController
 {
-    private $authors;
+    private $authorRepository;
 
-    public function __construct() {
-        $this->authors = [
-            [
-                'id' => 1,
-                'picture' => '/images/Victor-Hugo.jpg',
-                'username' => 'Victor Hugo',
-                'email' => 'victor.hugo@gmail.com',
-                'nb_books' => 100
-            ],
-            [
-                'id' => 2,
-                'picture' => '/images/william-shakespeare.jpg',
-                'username' => 'William Shakespeare',
-                'email' => 'william.shakespeare@gmail.com',
-                'nb_books' => 200
-            ],
-            [
-                'id' => 3,
-                'picture' => '/images/Taha_Hussein.jpg',
-                'username' => 'Taha Hussein',
-                'email' => 'taha.hussein@gmail.com',
-                'nb_books' => 300
-            ]
-        ];
+    public function __construct(AuthorRepository $authorRepository)
+    {
+        $this->authorRepository = $authorRepository;
     }
 
-    #[Route('/author/{id}', name: 'authorDetails ')]
-    public function authorDetails (int $id): Response
+    #[Route('/author/{id}', name: 'authorDetails')]
+    public function authorDetails(int $id): Response
     {
-        $author = null;
-        foreach ($this->authors as $a) {
-            if ($a['id'] === $id) {
-                $author = $a;
-                break;
-            }
-        }
+        $author = $this->authorRepository->find($id);
 
         if (!$author) {
             return new Response('Auteur non trouvé', 404);
@@ -56,18 +30,32 @@ class AuthorController extends AbstractController
         ]);
     }
 
-    #[Route('/listsauthors', name: 'list_authors')]
-public function listAuthors(): Response
-{
-    return $this->render('author/list.html.twig', [
-        'authors' => $this->authors,
-    ]);
-}
+    #[Route('/listauthors', name: 'list_authors')]
+    public function listAuthors(): Response
+    {
+        $authors = $this->authorRepository->findAll();
+        return $this->render('author/list.html.twig', [
+            'authors' => $authors, // Pass as 'authors'
+        ]);
+    }
 
+    #[Route('/findauthor/{id}', name: 'findByid')]
+    public function findByid(int $id): Response
+    {
+        $author = $this->authorRepository->find($id);
 
-#[Route('/listsauthors', name: 'goToAuthors')]
+        if (!$author) {
+            return new Response('Auteur non trouvé', 404);
+        }
+
+        return $this->render('author/authorDetails.html.twig', [
+            'author' => $author,
+        ]);
+    }
+
+    #[Route('/goToAuthors', name: 'goToAuthors')]
     public function goToAuthors(): Response
     {
-        return $this->redirectToRoute('author/list.html.twig');  
+        return $this->redirectToRoute('list_authors');
     }
 }
